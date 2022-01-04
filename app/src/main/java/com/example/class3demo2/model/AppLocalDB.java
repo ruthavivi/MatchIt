fc6aca41a6fab667fc02b6ad966f63bd3ccb0a97
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-01-04 19:46:17 +0200

    delete from fierbase not from list view

--- app/src/main/java/com/example/class3demo2/model/AppLocalDB.java
+++ app/src/main/java/com/example/class3demo2/model/AppLocalDB.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.class3demo2.MyApplication;
 
-@Database(entities = {Teacher.class}, version = 6)
+@Database(entities = {Teacher.class}, version = 7)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract TeacherDao teacherDao();
 }
