84afd14243329817384e8e3691fea3b2eb6c9405
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-01-13 19:06:04 +0200

    gooddddd

--- app/src/main/java/com/example/class3demo2/model/AppLocalDB.java
+++ app/src/main/java/com/example/class3demo2/model/AppLocalDB.java
@@ -6,7 +6,7 @@ import androidx.room.RoomDatabase;
 
 import com.example.class3demo2.MyApplication;
 
-@Database(entities = {Teacher.class}, version = 20)
+@Database(entities = {Teacher.class}, version = 21)
 abstract class AppLocalDbRepository extends RoomDatabase {
     public abstract TeacherDao teacherDao();
 }
